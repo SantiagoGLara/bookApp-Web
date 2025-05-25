53ae2c2dd9a327888b6c19862f6d8c84470d27a5
SantiagoGLara <relived57@gmail.com>
2025-05-25 00:19:20 -0600

    estructura general mis partes/ falta arreglar boton delete-edit en provider y supongo que por consiguiente el resto, falta arreglar css de los formularios.

--- base/content.php
+++ base/content.php
@@ -3,10 +3,24 @@
 $option=$_GET['op']??'01';//01 es la primera vez en entrar
 if($user)
 	switch($option){
-		case '00':include('salir.php');break;
-		case '10':include('../content/books_list.php');break;
-		case '11':include('../content/book_new.php');break;
-		case '12':include('../content/book_modify.php');break;
-		case '13':include('../content/book_delete.php');break;
+		case '00':include('salir.php');
+		break;
+		case '10':include('../content/books_list.php');
+		break;
+		case '11':include('../content/book_new.php');
+		break;
+		case '12':include('../content/book_modify.php');
+		break;
+		case '13':include('../content/book_delete.php');
+		break;
+		//cases ajenos a mi parte xd 
+		case '30':include('../content/provider.php');
+		break;
+		case '31':include('../content/provider_new.php');
+		break;//funciones provedores NEW
+		case '32':include('..//content/provider_modify.php');
+		break;//funciones provedores MODIFICAR
+		case '33':include('../content/provider_delete.php');
+		break;//funciones provedores DELETE
 	}
 ?>
---
+++ content/provider.php
@@ -0,0 +1,49 @@
+<?php
+    $consulta="SELECT nombre,numcelular,email,fecha_alta FROM proveedores order by  nombre";
+    $result=bd_consulta($consulta);
+?>
+<script type="text/javascript">
+  function asociarEventos(){
+    var botones=document.getElementsByClassName("botonBorrar");
+    for (var f = 0; f < botones.length; f++) {
+      var boton = botones[f];
+      boton.addEventListener("click", validar);
+    }
+  }
+  function validar(event){
+    if(!confirm("Estas seguro de eliminar este registro?"))
+      event.preventDefault();
+  }
+  window.addEventListener("load",asociarEventos);
+</script>
+<table>
+    <tr>
+        <th>#</th>        
+        <th>Nombre</th>        
+        <th>Numero Celular</th>        
+        <th>Email</th>        
+        <th>Fecha Alta</th>
+        <th><a href="../base/index.php?op=31">+ &#128218;</a></th><!-- ADD PROVEDOR  -->
+        <th></th><!-- VACIO, PERO ES COLUMNA MODIFICAR  -->
+    </tr>
+
+<?php
+    $i=0;
+    while($row=mysqli_fetch_assoc($result)){
+        $i++;
+?>
+<tr>
+    <td><?= $i ?></td>
+    <td><?= $row['nombre'] ?></td>
+    <td><?= $row['numcelular'] ?></td>
+    <td><?= $row['email'] ?></td>
+    <td><?= $row['fecha_alta'] ?></td>
+    <td><a class="botonBorrar" href="../base/index.php ?op=33 & id=<?= $row['id'] ?>">&#128465;</a></td>
+    <td><a href="../base/index.php?op=32&id=<?=$row['id']?>">&#9997;&#127995;</a></td>
+</tr>
+<?php } ?>
+<tr>
+    <td></td><td></td><td></td><td>El total de provedores es <?=$i?></td><td></td><td></td>
+</tr>
+</table>
+
