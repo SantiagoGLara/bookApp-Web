45ea8035bc07d074a3c929af1c80d1109d5c9ce8
SantiagoGLara <relived57@gmail.com>
2025-05-28 08:52:46 -0600

    todas las columnas de proveedor pueden tener ordenamiento

--- content/proveedores/provider.php
+++ content/proveedores/provider.php
@@ -3,65 +3,60 @@
     $result=bd_consulta($consulta);
 ?>
 <script type="text/javascript">
-    // Esta función se ejecuta cuando la página ha cargado completamente.
+    var nombreAsc = true;
+    var numCelularAsc = true;
+    var emailAsc = true;
+    var fechaAltaAsc = true;
+
     function asociarEventos() {
-        // Obtenemos el encabezado de la columna "Nombre" directamente por su ID.
-        // Esto es más seguro que usar getElementsByTagName("th")[1] porque evita problemas si el orden de los TH cambia.
-        var nombreHeader = document.getElementById("nombreColumna"); // Asegúrate de que este ID esté en tu TH de "Nombre"
-        if (nombreHeader) { // Verificamos que el elemento exista antes de intentar añadir el evento
-            nombreHeader.addEventListener("click", ordenarNombre);
-        } else {
-            console.error("El elemento con ID 'nombreColumna' no fue encontrado.");
+        var thNombre = document.getElementById("nombreColumna");
+        var thNumCelular = document.getElementById("numCelularColumna");
+        var thEmail = document.getElementById("emailColumna");
+        var thFechaAlta = document.getElementById("fechaAltaColumna");
+
+        if (thNombre) {
+            thNombre.addEventListener("click", ordenarPorNombre);
+        }
+        if (thNumCelular) {
+            thNumCelular.addEventListener("click", ordenarPorNumCelular);
+        }
+        if (thEmail) {
+            thEmail.addEventListener("click", ordenarPorEmail);
+        }
+        if (thFechaAlta) {
+            thFechaAlta.addEventListener("click", ordenarPorFechaAlta);
         }
     }
 
-    // Variable para controlar el orden de la ordenación (ascendente/descendente)
-    var nombreAsc = true;
-
-    function ordenarNombre() {
-        var tabla = document.getElementsByTagName("table")[0]; // La primera tabla en el documento
+    function ordenarPorNombre() {
+        var tabla = document.getElementsByTagName("table")[0];
         var filas = tabla.getElementsByTagName("tr");
 
-        // Los datos empiezan en la segunda fila (índice 1) y la última fila es la de totales.
-        var inicio = 1; // Saltar la fila de la cabecera (<th>)
-        // Restamos 2 para la fila de totales, ya que las filas de datos empiezan en el índice 1, y la última es de totales.
-        // Si tienes una sola fila de totales al final, entonces `filas.length - 1` es el último TR.
-        // Si tu última fila NO es parte de los datos a ordenar, `filas.length - 2` es correcto para el último dato.
-        // Si la última fila es 'El total de provedores es...', entonces el último dato es `filas.length - 2`.
-        var fin = filas.length - 1; // Evitar la última fila de totales
+        var inicio = 1;
+        var fin = filas.length - 1;
+        var numColumnasDatos = 6;
 
-        // Número de columnas en la tabla (asumiendo que es fijo)
-        var columnas = 6; // Ajusta esto al número real de columnas de datos en tus TR (incluyendo el último <td> del enlace)
-
-        // Creamos una matriz para almacenar los datos de las filas, excluyendo cabecera y totales
         var matriz = new Array(fin - inicio);
 
-        // Llenar la matriz con los datos de la tabla
         for (var i = inicio; i < fin; i++) {
-            matriz[i - inicio] = new Array(columnas);
+            matriz[i - inicio] = new Array(numColumnasDatos);
             var celdas = filas[i].getElementsByTagName("td");
-            for (var j = 0; j < columnas; j++) {
-                // Asegúrate de que la celda exista antes de intentar acceder a innerHTML
+            for (var j = 0; j < numColumnasDatos; j++) {
                 if (celdas[j]) {
                     matriz[i - inicio][j] = celdas[j].innerHTML;
                 } else {
-                    matriz[i - inicio][j] = ""; // Asignar vacío si la celda no existe
+                    matriz[i - inicio][j] = "";
                 }
             }
         }
 
-        // Ordenar la matriz por la columna "Nombre" (que es el índice 1, ya que "#" es el 0)
-        // Usamos un algoritmo de burbuja simple para ordenar
         for (var i = 0; i < matriz.length - 1; i++) {
             for (var j = i + 1; j < matriz.length; j++) {
-                // Convertimos a minúsculas para una ordenación insensible a mayúsculas/minúsculas
-                var a = matriz[i][1].toLowerCase(); // Columna del nombre (índice 1)
-                var b = matriz[j][1].toLowerCase(); // Columna del nombre (índice 1)
+                var a = matriz[i][1].toLowerCase();
+                var b = matriz[j][1].toLowerCase();
 
-                // Determinamos la condición de ordenación (ascendente o descendente)
                 var condicion = nombreAsc ? (a > b) : (a < b);
                 if (condicion) {
-                    // Intercambiar filas si la condición se cumple
                     var temp = matriz[i];
                     matriz[i] = matriz[j];
                     matriz[j] = temp;
@@ -69,27 +64,202 @@
             }
         }
 
-        // Reescribir las filas de la tabla con los datos ordenados
         for (var i = inicio; i < fin; i++) {
             var celdas = filas[i].getElementsByTagName("td");
-            for (var j = 0; j < columnas; j++) {
-                if (celdas[j]) { // Verificamos que la celda exista
+            for (var j = 0; j < numColumnasDatos; j++) {
+                if (celdas[j]) {
                     celdas[j].innerHTML = matriz[i - inicio][j];
                 }
             }
         }
 
-        // Cambiar la dirección de la flecha para indicar el nuevo orden
-        var flecha = document.getElementById("flechaNombre");
-        if (flecha) {
-            flecha.innerHTML = nombreAsc ? "▲" : "▼"; // Si estaba ascendente, ahora la flecha apunta hacia arriba
+        var flechas = document.querySelectorAll("span[id^='flecha']");
+        flechas.forEach(function(flecha) {
+            flecha.innerHTML = "▼";
+        });
+
+        var flechaNombre = document.getElementById("flechaNombre");
+        if (flechaNombre) {
+            flechaNombre.innerHTML = nombreAsc ? "▲" : "▼";
         }
 
-        // Invertir el estado de ordenación para el próximo clic
         nombreAsc = !nombreAsc;
     }
 
-    // Cuando la ventana haya cargado completamente, se asocian los eventos
+    function ordenarPorNumCelular() {
+        var tabla = document.getElementsByTagName("table")[0];
+        var filas = tabla.getElementsByTagName("tr");
+
+        var inicio = 1;
+        var fin = filas.length - 1;
+        var numColumnasDatos = 6;
+
+        var matriz = new Array(fin - inicio);
+
+        for (var i = inicio; i < fin; i++) {
+            matriz[i - inicio] = new Array(numColumnasDatos);
+            var celdas = filas[i].getElementsByTagName("td");
+            for (var j = 0; j < numColumnasDatos; j++) {
+                if (celdas[j]) {
+                    matriz[i - inicio][j] = celdas[j].innerHTML;
+                } else {
+                    matriz[i - inicio][j] = "";
+                }
+            }
+        }
+
+        for (var i = 0; i < matriz.length - 1; i++) {
+            for (var j = i + 1; j < matriz.length; j++) {
+                var a = parseFloat(matriz[i][2]);
+                var b = parseFloat(matriz[j][2]);
+
+                var condicion = numCelularAsc ? (a > b) : (a < b);
+                if (condicion) {
+                    var temp = matriz[i];
+                    matriz[i] = matriz[j];
+                    matriz[j] = temp;
+                }
+            }
+        }
+
+        for (var i = inicio; i < fin; i++) {
+            var celdas = filas[i].getElementsByTagName("td");
+            for (var j = 0; j < numColumnasDatos; j++) {
+                if (celdas[j]) {
+                    celdas[j].innerHTML = matriz[i - inicio][j];
+                }
+            }
+        }
+
+        var flechas = document.querySelectorAll("span[id^='flecha']");
+        flechas.forEach(function(flecha) {
+            flecha.innerHTML = "▼";
+        });
+
+        var flechaNumCelular = document.getElementById("flechaNumCelular");
+        if (flechaNumCelular) {
+            flechaNumCelular.innerHTML = numCelularAsc ? "▲" : "▼";
+        }
+
+        numCelularAsc = !numCelularAsc;
+    }
+
+    function ordenarPorEmail() {
+        var tabla = document.getElementsByTagName("table")[0];
+        var filas = tabla.getElementsByTagName("tr");
+
+        var inicio = 1;
+        var fin = filas.length - 1;
+        var numColumnasDatos = 6;
+
+        var matriz = new Array(fin - inicio);
+
+        for (var i = inicio; i < fin; i++) {
+            matriz[i - inicio] = new Array(numColumnasDatos);
+            var celdas = filas[i].getElementsByTagName("td");
+            for (var j = 0; j < numColumnasDatos; j++) {
+                if (celdas[j]) {
+                    matriz[i - inicio][j] = celdas[j].innerHTML;
+                } else {
+                    matriz[i - inicio][j] = "";
+                }
+            }
+        }
+
+        for (var i = 0; i < matriz.length - 1; i++) {
+            for (var j = i + 1; j < matriz.length; j++) {
+                var a = matriz[i][3].toLowerCase();
+                var b = matriz[j][3].toLowerCase();
+
+                var condicion = emailAsc ? (a > b) : (a < b);
+                if (condicion) {
+                    var temp = matriz[i];
+                    matriz[i] = matriz[j];
+                    matriz[j] = temp;
+                }
+            }
+        }
+
+        for (var i = inicio; i < fin; i++) {
+            var celdas = filas[i].getElementsByTagName("td");
+            for (var j = 0; j < numColumnasDatos; j++) {
+                if (celdas[j]) {
+                    celdas[j].innerHTML = matriz[i - inicio][j];
+                }
+            }
+        }
+
+        var flechas = document.querySelectorAll("span[id^='flecha']");
+        flechas.forEach(function(flecha) {
+            flecha.innerHTML = "▼";
+        });
+
+        var flechaEmail = document.getElementById("flechaEmail");
+        if (flechaEmail) {
+            flechaEmail.innerHTML = emailAsc ? "▲" : "▼";
+        }
+
+        emailAsc = !emailAsc;
+    }
+
+    function ordenarPorFechaAlta() {
+        var tabla = document.getElementsByTagName("table")[0];
+        var filas = tabla.getElementsByTagName("tr");
+
+        var inicio = 1;
+        var fin = filas.length - 1;
+        var numColumnasDatos = 6;
+
+        var matriz = new Array(fin - inicio);
+
+        for (var i = inicio; i < fin; i++) {
+            matriz[i - inicio] = new Array(numColumnasDatos);
+            var celdas = filas[i].getElementsByTagName("td");
+            for (var j = 0; j < numColumnasDatos; j++) {
+                if (celdas[j]) {
+                    matriz[i - inicio][j] = celdas[j].innerHTML;
+                } else {
+                    matriz[i - inicio][j] = "";
+                }
+            }
+        }
+
+        for (var i = 0; i < matriz.length - 1; i++) {
+            for (var j = i + 1; j < matriz.length; j++) {
+                var a = matriz[i][4];
+                var b = matriz[j][4];
+
+                var condicion = fechaAltaAsc ? (a > b) : (a < b);
+                if (condicion) {
+                    var temp = matriz[i];
+                    matriz[i] = matriz[j];
+                    matriz[j] = temp;
+                }
+            }
+        }
+
+        for (var i = inicio; i < fin; i++) {
+            var celdas = filas[i].getElementsByTagName("td");
+            for (var j = 0; j < numColumnasDatos; j++) {
+                if (celdas[j]) {
+                    celdas[j].innerHTML = matriz[i - inicio][j];
+                }
+            }
+        }
+
+        var flechas = document.querySelectorAll("span[id^='flecha']");
+        flechas.forEach(function(flecha) {
+            flecha.innerHTML = "▼";
+        });
+
+        var flechaFechaAlta = document.getElementById("flechaFechaAlta");
+        if (flechaFechaAlta) {
+            flechaFechaAlta.innerHTML = fechaAltaAsc ? "▲" : "▼";
+        }
+
+        fechaAltaAsc = !fechaAltaAsc;
+    }
+
     window.addEventListener("load", asociarEventos);
 </script>
 
@@ -97,9 +267,9 @@
     <tr>
         <th>#</th>
         <th id="nombreColumna" style="cursor:pointer">Nombre <span id="flechaNombre">▼</span></th>
-        <th>Numero Celular</th>
-        <th>Email</th>
-        <th>Fecha Alta</th>
+        <th id="numCelularColumna" style="cursor:pointer">Numero Celular <span id="flechaNumCelular">▼</span></th>
+        <th id="emailColumna" style="cursor:pointer">Email <span id="flechaEmail">▼</span></th>
+        <th id="fechaAltaColumna" style="cursor:pointer">Fecha Alta <span id="flechaFechaAlta">▼</span></th>
         <th><a href="../base/index.php?op=31">+ &#128218;</a></th>
     </tr>
 
