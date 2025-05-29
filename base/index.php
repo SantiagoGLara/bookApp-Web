c95a5d55a3d101932c7c298a1bbc4b1a4b1e3b1e
SantiagoGLara <relived57@gmail.com>
2025-05-28 23:37:27 -0600

    pantalla de bienvenida

--- base/index.php
+++ base/index.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html>
-	<?php include('head.php'); ?>
-	<body>
-		<?php
-		include('header.php');
-		include('menu.php');
-		if(!$user) include('../content/authenticate.php');
-		?>
-		<main>
-			<?php include('content.php'); ?>
-		</main>
-			<?php include('footer.php'); ?>
-	</body>
+<?php include('head.php'); ?>
+<body>
+    <?php
+    include('header.php');
+    include('menu.php');
+    if (!$user) {
+        include('../content/authenticate.php');
+    } else {
+        include('../base/pantallabienvenida.php');
+    }
+
+
+    ?>
+    <main>
+        <?php include('content.php'); ?>
+    </main>
+    <?php include('footer.php'); ?>
+</body>
 </html>
---
+++ base/pantallabienvenida.php
@@ -0,0 +1,39 @@
+<div class=bienvenida>
+    <h1 class="titulo_bienvenida">Bienvenido al Sistema de Compra de libros</h1>
+    <h2 class="titulo_bienvenida">Vista del administrador</h2>
+    <h3 class="titulo_bienvenida">Página en la que usted podrá añadir,visualizar,editar y/o eliminar los siguientes aspectos</h3>
+</div>
+<main class="funciones">
+    <div class="funcion">
+        <img src="../images/InicioBook.png" alt="Libros">
+        <p>Libros</p>
+    </div>
+    <div class="funcion">
+        <img src="../images/inicioClientes.png" alt="Clientes">
+        <p>Clientes</p>
+    </div>
+    <div class="funcion">
+        <img src="../images/inicioProveedores.png" alt="Proveedores">
+        <p>Proveedores</p>
+    </div>
+    <div class="funcion">
+        <img src="../images/inicioCompras.png" alt="Compras">
+        <p>Compras</p>
+    </div>
+    <div class="funcion">
+        <img src="../images/inicioVentas.png" alt="Ventas">
+        <p>Ventas</p>
+    </div>
+    <div class="funcion">
+        <img src="../images/inicioUsuarios.png" alt="Usuarios">
+        <p>Usuarios</p>
+    </div>
+    <div class="funcion">
+        <img src="../images/inicioPaises.png" alt="Usuarios">
+        <p>Paises</p>
+    </div>
+    <div class="funcion">
+        <img src="../images/inicioLenguajes.png " alt="lenguajes">
+        <p>lenguajes</p>
+    </div>
+</main>
